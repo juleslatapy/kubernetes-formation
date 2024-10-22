dcc264f7fb890f6c70388bd022a404b8ca3a02b0
juleslatapy <jules.latapy@gmail.com>
2024-10-22 16:46:49 +0200

    new version app

--- kubernetes/fluxcd/repositories/example-app-1/src/app.py
+++ kubernetes/fluxcd/repositories/example-app-1/src/app.py
@@ -3,4 +3,25 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello World! v1.0.0.2 seconde version"
+    return '''
+    <!doctype html>
+    <html lang="fr">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <title>Hello World</title>
+        <style>
+            body {
+                background-color: red; /* Couleur de fond rouge */
+                color: white; /* Couleur du texte */
+                text-align: center; /* Centre le texte */
+                font-family: Arial, sans-serif; /* Police */
+                padding: 50px; /* Ajoute du padding */
+            }
+        </style>
+    </head>
+    <body>
+        <h1>Hello World! v1.0.0.2 seconde version</h1>
+    </body>
+    </html>
+    '''
